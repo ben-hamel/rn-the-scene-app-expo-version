31d0ba92c6076577e486142d00c3b15bfca52868
Ben Hamel <benehamel@gmail.com>
2022-06-30 13:34:21 -0230

    remove unneed code

--- screens/CategoryDetailScreen.js
+++ screens/CategoryDetailScreen.js
@@ -1,23 +1,25 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import SkillFeed from '../components/SkillFeed'
+import SkillFeed from "../components/SkillFeed";
 
 export default function CategoryDetailScreen({ route, navigation }) {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>CategoryDetailScreen</Text>
-            <Text>route name: {route.params}</Text>
-            <SkillFeed skill={route.params} />
-            <Button title="Go back" onPress={() => navigation.goBack()} />
-        </SafeAreaView>
-    )
+  return (
+    // <SafeAreaView style={styles.container}>
+    <>
+      <Text>CategoryDetailScreen</Text>
+      <Text>route name: {route.params}</Text>
+      <SkillFeed skill={route.params} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </>
+    // {/* </SafeAreaView> */}
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // borderColor: 'red',
-        // borderWidth: 1,
-        // borderStyle: 'solid',
-    }
+  container: {
+    flex: 1,
+    // borderColor: 'red',
+    // borderWidth: 1,
+    // borderStyle: 'solid',
+  },
 });
