c8cdd7d7997ce55c4a293148e1ce4061d083de11
Ben Hamel <benehamel@gmail.com>
2022-06-30 13:33:56 -0230

    removed comment and restyled component

--- components/SkillFeed/SkillFeed.js
+++ components/SkillFeed/SkillFeed.js
@@ -1,113 +1,93 @@
-import {
-    View,
-    Text,
-    FlatList,
-    StyleSheet,
-    Item,
-    Button,
-    Image,
-    Pressable,
-} from "react-native";
+import { View, Text, FlatList, StyleSheet, Image } from "react-native";
 import React, { useState, useEffect } from "react";
 import { db } from "../../config/firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 const SkillFeed = ({ skill }) => {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    /**
-     * import users from the choosen skill
-     */
-    useEffect(() => {
-        const getData = async () => {
-            const q = query(
-                collection(db, "users"),
-                where("skill", "array-contains-any", [skill])
-            );
-            const querySnapshot = await getDocs(q);
-            const data = querySnapshot.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
-            setUsers(data);
-        };
-
-        getData();
-    }, []);
-
-    /**
-     * ListItem Component for the FlatList
-     */
-    const ListItem = ({ item }) => {
-        return (
-            <View style={styles.listItem}>
-                <Image
-                    source={{
-                        uri: item.profile_picture,
-                    }}
-                    style={styles.itemPhoto}
-                    resizeMode="cover"
-                />
-                <Text style={styles.itemText}>{item.username}</Text>
-            </View>
-        );
+  /**
+   * import users from the choosen skill
+   */
+  useEffect(() => {
+    const getData = async () => {
+      const q = query(
+        collection(db, "users"),
+        where("skill", "array-contains-any", [skill])
+      );
+      const querySnapshot = await getDocs(q);
+      const data = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setUsers(data);
     };
 
+    getData();
+  }, []);
 
+  /**
+   * ListItem Component for the FlatList
+   */
+  const ListItem = ({ item }) => {
     return (
-        <View>
-            <Text>SkillFeed</Text>
-            <Text>Skill:{skill}</Text>
-            <FlatList
-                // horizontal
-                data={users}
-                // keyExtractor={(item) => item.id}
-                renderItem={({ item }) => <ListItem item={item} />}
-                showsHorizontalScrollIndicator={false}
-            />
+      <View style={styles.listItem}>
+        <Image
+          source={{
+            uri: item.profile_picture,
+          }}
+          style={styles.itemPhoto}
+          resizeMode="cover"
+        />
+        <View style={styles.itemInfo}>
+          <Text style={styles.itemText}>{item.username}</Text>
         </View>
-    )
-}
+      </View>
+    );
+  };
 
-export default SkillFeed
+  return (
+    <View>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => <ListItem item={item} />}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default SkillFeed;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // alignItems: "center",
-        // justifyContent: "center",
-        // backgroundColor: "#121212",
-    },
-    sectionHeader: {
-        fontWeight: "800",
-        fontSize: 18,
-        // color: "#f4f4f4",
-        marginTop: 20,
-        marginLeft: 10,
-        marginBottom: 5,
-    },
-    listItem: {
-        margin: 10,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        backgroundColor: "gray",
-    },
-    itemPhoto: {
-        width: 120,
-        height: 120,
-    },
-    itemText: {
-        // color: "rgba(255, 255, 255, 0.5)",
-        marginTop: 5,
-    },
-    button: {
-        width: "70%",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 24,
-        backgroundColor: "blue",
-        padding: 10,
-        borderRadius: 8,
-    },
-    buttonText: { color: "white", fontSize: 18 },
+  container: {
+    flex: 1,
+  },
+
+  listItem: {
+    flexDirection: "row",
+    padding: 10,
+    margin: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  itemPhoto: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+  },
+  itemText: {
+    marginTop: 5,
+  },
+  itemInfo: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+  },
 });
