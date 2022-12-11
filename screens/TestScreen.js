621b6ef35135fef750e36bce6a10f6444542764d
Ben Hamel <benehamel@gmail.com>
2022-12-11 12:05:36 -0330

    play one video at a time

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useRef, useCallback } from "react";
+import { View, Button, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function TestScreen() {
+export default function TestScreen({ navigation, route }) {
   return (
-    <View>
-      <Text>TestScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View>
+          <Text>Test</Text>
+          <Button title="Go back" onPress={() => navigation.goBack()} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- screens/UserDetailScreen.js
+++ screens/UserDetailScreen.js
@@ -1,12 +1,39 @@
-import { Text, Button, StyleSheet, Dimensions, Animated } from "react-native";
-import React, { useRef } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Dimensions,
+  Animated,
+  Pressable,
+} from "react-native";
+import { Video } from "expo-av";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 
 const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+// const windowHeight = Dimensions.get("window").height;
 
 const UserDetailScreen = ({ navigation, route }) => {
   /* Animated Header */
   const pan = useRef(new Animated.ValueXY()).current;
+
+  /* MOCK DATA */
+  const videos = [
+    {
+      id: 1,
+      title: "Video 1",
+      url: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+    },
+    {
+      id: 2,
+      title: "Video 2",
+      url: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+    },
+  ];
+
+  const videoRefs = useRef([]);
+
+  const [activeVideo, setActiveVideo] = React.useState(null);
 
   return (
     <Animated.ScrollView
@@ -26,44 +53,78 @@ const UserDetailScreen = ({ navigation, route }) => {
           transform: [
             {
               translateY: pan.y.interpolate({
-                inputRange: [-1000, 0],
+                inputRange: [-200, 0],
                 outputRange: [-150, 0],
                 extrapolate: "clamp",
               }),
             },
             {
               scale: pan.y.interpolate({
-                inputRange: [-3000, 0],
-                outputRange: [20, 1],
+                inputRange: [-200, 0],
+                outputRange: [2, 1],
                 extrapolate: "clamp",
               }),
             },
           ],
-          height: windowHeight / 3,
-          width: windowWidth,
+          height: 300, // specify the height of the image explicitly
+          width: "100%", // the image should fill the width of its
         }}
       />
-      <Animated.View
+      <Text style={styles.textOverlay}>My Text Overlay</Text>
+
+      {/*  VIDEOS */}
+      <View
         style={{
-          paddingHorizontal: 16,
-          transform: [
-            {
-              translateY: pan.y.interpolate({
-                inputRange: [-1000, 0],
-                outputRange: [48 * 20, 0],
-                extrapolate: "clamp",
-              }),
-            },
-          ],
+          alignItems: "center",
         }}
       >
         <Text>UserDetailScreen</Text>
+        <Button
+          title="Test"
+          onPress={() => navigation.navigate("TestScreen")}
+        />
         <Text>route name: {route.params}</Text>
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </Animated.View>
-      {/* <Text>UserDetailScreen</Text>
-      <Text>route name: {route.params}</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} /> */}
+        {videos.map((video, index) => {
+          videoRefs.current[index] = React.createRef();
+          return (
+            <View key={video.id}>
+              <Video
+                key={video.id}
+                ref={videoRefs.current[index]}
+                source={{ uri: video.url }}
+                // paused={currentVideo ? currentVideo.id !== video.id : false}
+                resizeMode="cover"
+                shouldPlay={activeVideo === video.id}
+                // onPlaybackStatusUpdate={(status) => {
+                //   console.log(status);
+                //   console.log("activevi", activeVideo);
+                //   console.log("video id", video.id);
+                // }}
+                useNativeControls
+                style={styles.video}
+              />
+              <Button
+                key={`button${video.id}`}
+                title={activeVideo === video.id ? "stop" : "play"}
+                onPress={() => {
+                  //if activevideo is equal to video id, stop playing immediately else set active video to video id
+                  if (activeVideo === video.id) {
+                    setActiveVideo(null);
+                  } else {
+                    setActiveVideo(video.id);
+                  }
+                }}
+              />
+              <Text>{activeVideo}</Text>
+            </View>
+          );
+        })}
+        <Button
+          title="Test"
+          onPress={() => navigation.navigate("TestScreen")}
+        />
+      </View>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </Animated.ScrollView>
   );
 };
@@ -71,12 +132,27 @@ const UserDetailScreen = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: "100%",
   },
   img: {
     width: windowWidth,
-    height: "33.3%",
-    // resizeMode: "contain",
+    height: 150,
+  },
+  textOverlay: {
+    position: "absolute", // use absolute positioning
+    zIndex: 1, // set the z-index to ensure the text appears above the image
+    top: 230, // position the text at the top of the container
+    left: 30, // position the text at the left of the container
+    justifyContent: "center", // center the text vertically within the container
+    alignItems: "center", // center the text horizontally within the container
+    fontSize: 24, // set the font size of the text
+    color: "white", // set the color of the text
+    fontWeight: "bold",
+  },
+  video: {
+    width: 300,
+    height: 200,
+    marginBottom: 20,
+    marginTop: 10,
   },
 });
 
