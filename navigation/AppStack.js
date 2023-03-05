ccb99fa62f532ed1ab98d13732aa9786e18712e5
Ben Hamel <benehamel@gmail.com>
2023-03-05 17:06:30 -0330

    add tab bar to bottom of screen

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import {
   HomeScreen,
@@ -9,23 +10,52 @@ import {
   UserDetailScreen,
 } from "../screens";
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export const AppStack = () => {
+function HomeStackScreen() {
   return (
-    <Stack.Navigator
+    <HomeStack.Navigator
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="TestScreen" component={TestScreen} />
-      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-      <Stack.Screen
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      {/* <HomeStack.Screen name="TestScreen" component={TestScreen} /> */}
+      {/* <HomeStack.Screen name="ProfileScreen" component={ProfileScreen} /> */}
+      <HomeStack.Screen
         name="CategoryDetailScreen"
         component={CategoryDetailScreen}
       />
-      <Stack.Screen name="UserDetailScreen" component={UserDetailScreen} />
-    </Stack.Navigator>
+      <HomeStack.Screen name="UserDetailScreen" component={UserDetailScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+export const AppStack = () => {
+  return (
+    // <Stack.Navigator
+    //   screenOptions={{
+    //     headerShown: false,
+    //   }}
+    // >
+    //   <Stack.Screen name="Home" component={HomeScreen} />
+    //   <Stack.Screen name="TestScreen" component={TestScreen} />
+    //   <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+    //   <Stack.Screen
+    //     name="CategoryDetailScreen"
+    //     component={CategoryDetailScreen}
+    //   />
+    //   <Stack.Screen name="UserDetailScreen" component={UserDetailScreen} />
+    // </Stack.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeStackScreen} />
+      <Tab.Screen name="TestScreen" component={TestScreen} />
+      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 };
