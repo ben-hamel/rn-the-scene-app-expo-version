ca75577dc2d0b35d01c04ebdf59e231bcd6d9237
Ben Hamel <benehamel@gmail.com>
2023-09-03 11:36:53 -0230

    add back and done buttons to edit skill screen

--- screens/CategoryDetailScreen.js
+++ screens/CategoryDetailScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Button } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SkillFeed from "../components/SkillFeed";
@@ -6,8 +6,6 @@ import SkillFeed from "../components/SkillFeed";
 export default function CategoryDetailScreen({ route, navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>CategoryDetailScreen</Text>
-      <Text>route name: {route.params}</Text>
       <SkillFeed skill={route.params} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </SafeAreaView>
