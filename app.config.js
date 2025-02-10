e573bbad288fa47bc79b88e802a78ebb877b6b69
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2025-02-10 16:11:57 -0330

    build: update to sdk 52

--- app.config.js
+++ app.config.js
@@ -4,6 +4,7 @@ export default {
   expo: {
     name: "rn-the-scene-app-expo-version",
     slug: "rn-the-scene-app-expo-version",
+    newArchEnabled: true,
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
--- utils/imagePicker.js
+++ utils/imagePicker.js
@@ -2,7 +2,7 @@ import * as ImagePicker from "expo-image-picker";
 
 export const pickImage = async () => {
   let result = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ImagePicker.MediaTypeOptions.All,
+    mediaTypes: ["images", "videos"],
     allowsEditing: true,
     aspect: [4, 3],
     // quality: 0,
